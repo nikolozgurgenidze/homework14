a089e56e60f42a1d847e8551d4134d0d3d5fbe42
nikolozgurgenidze <gurgenidze2007@gmail.com>
2023-01-30 18:40:47 +0400

    Create code.js

---
+++ code.js
@@ -0,0 +1,29 @@
+let number = "4"
+let number2 = "112"
+let number3 = "32"
+let number4 = "45"
+let number5 = "267"
+let numbers = [number, number2, number3, number4, number5]
+console.log (number + number2 + number3)
+
+
+let personsName = [
+    {
+        name: "nika",
+        age: "15",
+        address: "lech2",
+    },
+    {
+        name: "elene",
+        age: "25",
+        address: "bechankalandadzis3"
+    },
+    {
+        name: "gio",
+        age: "16",
+        address: "shavizgvis qucha"
+    }
+]
+console.log(personsName[0].name)
+console.log(`My age is ${personsName[1].age}`)
+// cpnsole.log(`My address is ${personsName[2].address}`)
